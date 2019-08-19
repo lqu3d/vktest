2afe0735529e608201b35bf09277a43d105fa434
liqi <2191617112@qq.com>
2019-08-19 12:55:11 +0800

    XTransform 旋转，缩放，量子观察法

--- examples/testx/XTransform.cpp
+++ examples/testx/XTransform.cpp
@@ -5,60 +5,157 @@ XTransform::XTransform(XGameObject* gameObject)
 	: gameObject(gameObject)
 	
 {
-	position = glm::vec3(0);
+	SetPos(0, 0, 0);
+	SetRot(0, 0, 0);
+	SetScale(1, 1, 1);
 
 }
 
-void XTransform::SetPosition(float x, float y, float z)
+void XTransform::SetPos(float x, float y, float z)
 {
 	position.x = x;
 	position.y = y;
 	position.z = z;
 
-	Refresh();
+	posChanged = true;
 }
 
-void XTransform::SetPosition(const glm::vec3& pos)
+void XTransform::SetPos(const glm::vec3& pos)
 {
 	position = pos;
-	Refresh();
-
+	posChanged = true;
 }
 
 void XTransform::SetX(float x)
 {
 	position.x = x;
-	Refresh();
-
+	posChanged = true;
 }
 
 void XTransform::SetY(float y)
 {
 	position.y = y;
-	Refresh();
-
+	posChanged = true;
 }
 
 void XTransform::SetZ(float z)
 {
 	position.z = z;
-	Refresh();
+	posChanged = true;
+}
+
+/***量子观察设计法
+* 只有当一个数据被观察时，才去更新它
+* 优点：可以整体上避免无用计算
+* 缺点：集中式GET会造成耗时波峰，可采用预先GET来解决
+*/
+const glm::vec3* XTransform::GetPosition()
+{
+	if (posChanged) {
+		RefreshHierachy();
+		posChanged = false;
+	}
+	return &position;
+}
+
+void XTransform::SetScale(float x, float y, float z)
+{
+	scale.x = x;
+	scale.y = y;
+	scale.z = z;
+	scaleChanged = true;
+}
+
+void XTransform::SetScale(const vec3& scale)
+{
+	this->scale = scale;
+	scaleChanged = true;
+}
+
+void XTransform::SetScaleX(float x)
+{
+	scale.x = x;
+	scaleChanged = true;
 
 }
 
-const glm::vec3* XTransform::GetPosition()
+void XTransform::SetScaleY(float y)
 {
-	return &position;
+	scale.y = y;
+	scaleChanged = true;
+
+}
+
+void XTransform::SetScaleZ(float z)
+{
+	scale.z = z;
+	scaleChanged = true;
+
+}
+
+const vec3* XTransform::GetScale()
+{
+	if (scaleChanged) {
+		RefreshHierachy();
+		scaleChanged = false;
+	}
+	return &scale;
+}
+
+void XTransform::SetRot(float x, float y, float z)
+{
+	rotation.x = x;
+	rotation.y = y;
+	rotation.z = z;
+	rotChanged = true;
+}
+
+void XTransform::SetRot(const vec3& rot)
+{
+	rotation = rot;
+	rotChanged = true;
+}
+
+void XTransform::SetRotX(float x)
+{
+	rotation.x = x;
+	rotChanged = true;
+}
+
+void XTransform::SetRotY(float y)
+{
+	rotation.y = y;
+	rotChanged = true;
+}
+
+void XTransform::SetRotZ(float z)
+{
+	rotation.z = z;
+	rotChanged = true;
+}
+
+const vec3* XTransform::GetRot()
+{
+	if (rotChanged) {
+		RefreshHierachy();
+		rotChanged = false;
+	}
+	return &rotation;
 }
 
 void XTransform::SetParent(XTransform* parent)
 {
-	Refresh();
+	this->parent = parent;
+	parentChanged = true;
 }
 
 XTransform* XTransform::GetParent()
 {
-	return nullptr;
+	if (parentChanged) {
+		RefreshHierachy();
+		parentChanged = false;
+	}
+	return parent;
 }
 
 void XTransform::LookAt(const vec3 & eye, const vec3 & center, const vec3& up)
@@ -72,8 +169,10 @@ const glm::mat4* XTransform::GetMatrix()
 }
 
 
-void XTransform::Refresh()
+void XTransform::RefreshHierachy()
 {
+	tmProj = glm::translate(tmView, position);
+
 	//update self
 	if (parent) {
 
--- examples/testx/XTransform.h
+++ examples/testx/XTransform.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #include<vector>
 #include <glm/glm.hpp>
@@ -19,26 +20,51 @@ private:
 	vec3 localRotation;
 	vec3 localScale;
 
+	vec3 up;
+	vec3 right;
+	vec3 forward;
+
 	mat4 tmView;
+	mat4 tmProj;
 	XTransform* parent;
 	std::vector<XTransform*> childrens;
+
+	bool posChanged = false;
+	bool rotChanged = false;
+	bool scaleChanged = false;
+	bool parentChanged = false;
 
 private:
 	//只能由GameObject和XComponent构造，不允许继承，不允许外部私自生成
 	XTransform(XGameObject* gameObject);
 
-	void Refresh();
+	void RefreshHierachy();
 public:
 
 	XGameObject* const gameObject;
 	/***
 	* 使用函数式设置的目的，数据变化得到监视，数据变化时才重新计算整个父子层级
-	* 可能的缺点是：集中式GET会造成耗时波峰，可采用预先GET来解决
 	*/
-	void SetPosition(float x, float y, float z);
-	void SetPosition(const glm::vec3& pos);
-	void SetX(float x); void SetY(float y); void SetZ(float z);
-	const glm::vec3* GetPosition();
+	void SetPos(float x, float y, float z);
+	void SetPos(const vec3& pos);
+	void SetX(float x); 
+	void SetY(float y); 
+	void SetZ(float z);
+	const vec3* GetPosition();
+
+	void SetScale(float x, float y, float z);
+	void SetScale(const vec3& scale);
+	void SetScaleX(float x);
+	void SetScaleY(float y);
+	void SetScaleZ(float z);
+	const vec3* GetScale();
+
+	void SetRot(float x, float y, float z);
+	void SetRot(const vec3& rot);
+	void SetRotX(float x);
+	void SetRotY(float y);
+	void SetRotZ(float z);
+	const vec3* GetRot();
 
 	void SetParent(XTransform* parent);
 	XTransform* GetParent();
@@ -46,5 +72,6 @@ public:
 	void LookAt(const vec3 & eye, const vec3 & center, const vec3& up);
 
 	const glm::mat4* GetMatrix();
+
 };
 
